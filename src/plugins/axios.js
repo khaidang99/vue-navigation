83d32e7dc9f30e74aa42d8ba1d99f1b3665af536
Canopus <khaidang2812@gmail.com>
2021-07-29 17:10:17 +0700

    refactor : axios plugins

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,34 +1,31 @@
-import axios from 'axios';
-import router from '@/router';
+import axios from "axios";
+import router from "@/router";
 
 const instance = axios.create();
-instance.defaults.baseURL = 'https://5f55a98f39221c00167fb11a.mockapi.io/';
-instance.defaults.headers.common.Accept = 'application/json';
-instance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+instance.defaults.baseURL = "https://5f55a98f39221c00167fb11a.mockapi.io/";
+instance.defaults.headers.common.Accept = "application/json";
+instance.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-instance.interceptors.response.use(response => response,
+instance.interceptors.response.use(
+  (response) => response,
   async (error) => {
     var { status } = error.response;
     if ([400, 404].includes(status)) {
-      router.app.$notification.error(
-        {
-          message: 'message_error',
-          description: `error_${status}_content`
-        }
-      )
-      return Promise.reject(error)
+      router.app.$notification.error({
+        message: "message_error",
+        description: `error_${status}_content`,
+      });
+      return Promise.reject(error);
     }
-    if([500].includes(status)) {
-      router.app.$notification.error(
-        {
-          message: 'message_error',
-          description: `error_${status}_content`
-        }
-      )
-      return Promise.reject(error)
+    if ([500].includes(status)) {
+      router.app.$notification.error({
+        message: "message_error",
+        description: `error_${status}_content`,
+      });
+      return Promise.reject(error);
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default instance
+export default instance;
